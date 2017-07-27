598a17738fde9eb04eb11d61ba74ef1872ccfc50
siczekkonrad <konradd.siczek@gmail.com>
2017-07-27 23:34:31 +0200

    added cpt display shortcode

--- functions.php
+++ functions.php
@@ -92,3 +92,251 @@ if(function_exists('register_sidebar') ){
             ));
 
         }
+
+
+add_filter( 'manage_posts_columns', 'revealid_add_id_column', 5 );
+add_action( 'manage_posts_custom_column', 'revealid_id_column_content', 5, 2 );
+
+
+function revealid_add_id_column( $columns ) {
+   $columns['revealid_id'] = 'ID';
+   return $columns;
+}
+
+function revealid_id_column_content( $column, $id ) {
+  if( 'revealid_id' == $column ) {
+    echo $id;
+  }
+}
+
+
+
+ function add_banners() {
+        $labels = array(
+            'name' => _x('Banners', 'post type general name'),
+            'singular_name' => _x('Banner', 'post type singular name'),
+            'add_new' => _x('Add New', 'Banner'),
+            'add_new_item' => __('Add New Banner'),
+            'edit_item' => __('Edit Banner'),
+            'new_item' => __('New Banner'),
+            'view_item' => __('View Banner'),
+            'search_items' => __('Search Banner'),
+            'not_found' =>  __('Nothing found'),
+            'not_found_in_trash' => __('Nothing found in Trash'),
+            'parent_item_colon' => ''
+        );
+        $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'publicly_queryable' => false,
+            'show_ui' => true,
+            'query_var' => true,
+            'menu_icon' => null,
+            'rewrite' => true,
+            'capability_type' => 'post',
+            'hierarchical' => false,
+            'menu_position' => null,
+            'register_meta_box_cb' => 'banner_link_meta_box',
+            'supports' => array('title', 'page-attributes','post-formats', 'excerpt', 'thumbnail'),
+            'taxonomies' => array('category_banners') 
+        ); 
+        register_post_type( 'banner' , $args );
+        register_taxonomy("categories", array("banner"), array("hierarchical" => true, "label" => "Categories", "singular_label" => "Category", "rewrite" => array( 'slug' => 'banner', 'with_front'=> false )));
+
+        }
+
+    add_action('init', 'add_banners');
+
+
+
+
+/**
+ * Adds a box to the main column on the Post and Page edit screens.
+ */
+function nw_add_custom_meta_box() {
+
+    $screens = array( 'site' ); // add items to add to multiple post types
+
+    foreach ( $screens as $screen ) {
+        add_meta_box(
+        'website', // $id
+        'Website', // $title 
+        'show_custom_meta_box', // $callback
+        $screen, // $page
+        'normal', // $context
+        'high' // $priority
+        );
+    }
+}
+
+function banner_link_meta_box() {
+
+    add_meta_box(
+        'banner-link',
+        __( 'Banner Link', 'tonikt' ),
+        'banner_link_meta_box_callback',
+        'banner'
+    );
+}
+
+add_action( 'add_meta_boxes', 'banner_link_meta_box' );
+
+
+function banner_link_meta_box_callback( $post ) {
+
+    // Add a nonce field so we can check for it later.
+    wp_nonce_field( 'banner_link_nonce', 'banner_link_nonce' );
+
+    $value = get_post_meta( $post->ID, '_banner_link', true );
+
+    echo '<textarea style="width:100%"  name="banner_link">' . esc_attr( $value ) . '</textarea>';
+}
+
+
+function save_banner_link_meta_box_data( $post_id ) {
+
+    // Check if our nonce is set.
+    if ( ! isset( $_POST['banner_link_nonce'] ) ) {
+        return;
+    }
+
+    // Verify that the nonce is valid.
+    if ( ! wp_verify_nonce( $_POST['banner_link_nonce'], 'banner_link_nonce' ) ) {
+        return;
+    }
+
+    // If this is an autosave, our form has not been submitted, so we don't want to do anything.
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
+
+    // Check the user's permissions.
+    if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+        if ( ! current_user_can( 'edit_page', $post_id ) ) {
+            return;
+        }
+
+    }
+    else {
+
+        if ( ! current_user_can( 'edit_post', $post_id ) ) {
+            return;
+        }
+    }
+
+    /* OK, it's safe for us to save the data now. */
+
+    // Make sure that it is set.
+    if ( ! isset( $_POST['banner_link'] ) ) {
+        return;
+    }
+
+    // Sanitize user input.
+    $my_data = sanitize_text_field( $_POST['banner_link'] );
+
+    // Update the meta field in the database.
+    update_post_meta( $post_id, '_banner_link', $my_data );
+}
+
+add_action( 'save_post', 'save_banner_link_meta_box_data' );
+
+
+
+add_shortcode( 'display_banner', 'display_banner_post_type' );
+
+    function display_banner_post_type($atts){
+    	global $post;
+
+        $default = array(
+            'post_type' => 'banner',
+            'p'=> '',
+            'category_name'=> ''
+        );
+
+        $r = shortcode_atts( $default, $atts);
+        extract( $r );
+
+        $string = '';
+        $query = new WP_Query( $r );
+        if( $query->have_posts() ){
+        	$string .= '<div class="banner">';
+            while( $query->have_posts() ){
+                $query->the_post();
+                if($r['category_name'] == 'download') {
+                	$icon = 'download';
+                }elseif($r['category_name'] == 'information'){
+                	$icon = 'settings';
+                }
+                $string .=  '<div class="banner__wrapper">';
+                $string .=  '<div class="banner__icon">';
+                $string .=  '<img src="'. get_template_directory_uri() .'/assets/icon-'. $icon .'.svg"/>';
+                $string .=  '</div>';
+                $string .=  '<div class="banner__content">';
+                $string .=  '<a class="banner__title" href="' . get_post_meta( $post->ID, '_banner_link', true ) . '"> '.get_the_title(). '</a>';
+                $string .=  '<span class="banner__text">' . get_the_excerpt() . '</span>' ;
+                
+                $string .=  '</div>';    
+            }
+            $string .= '</div>';
+            wp_reset_postdata();
+        }
+        return $string;
+        wp_reset_query();
+    }
+
+
+/// options
+    function theme_settings_page(){
+	    ?>
+		    <div class="wrap">
+		    <h1>Theme Panel</h1>
+		    <form method="post" action="options.php">
+		        <?php
+		            settings_fields("section");
+		            do_settings_sections("theme-options");      
+		            submit_button(); 
+		        ?>          
+		    </form>
+			</div>
+		<?php
+	}
+function add_theme_menu_item(){
+	add_menu_page("Theme Panel", "Theme Panel", "manage_options", "theme-panel", "theme_settings_page", null, 99);
+}
+
+add_action("admin_menu", "add_theme_menu_item");
+
+    function display_twitter_element(){
+	?>
+		<input type="checkbox" name="twitter" value="1" <?php checked(1, get_option('twitter'), true); ?> /> 
+	<?php
+	}
+
+	function display_facebook_element(){
+	?>
+		<input type="checkbox" name="facebook" value="1" <?php checked(1, get_option('facebook'), true); ?> /> 
+	<?php
+	}
+
+	function display_linkedin_element(){
+	?>
+		<input type="checkbox" name="linkedin" value="1" <?php checked(1, get_option('linkedin'), true); ?> /> 
+	<?php
+	}
+   
+
+   function display_theme_panel_fields()
+{
+	add_settings_section("section", "All Settings", null, "theme-options");
+	
+	add_settings_field("twitter", "Do you want share on Twiiter?", "display_twitter_element", "theme-options", "section");
+    add_settings_field("facebook", "Do you want share on Facebook?", "display_facebook_element", "theme-options", "section");
+    add_settings_field("linkedin", "Do you want share on Linkedin?", "display_linkedin_element", "theme-options", "section");
+
+    register_setting("section", "twitter");
+    register_setting("section", "facebook");
+    register_setting("section", "linkedin");
+}
+
+add_action("admin_init", "display_theme_panel_fields");
